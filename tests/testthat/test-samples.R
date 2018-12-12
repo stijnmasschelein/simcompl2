81a8ba59a58cea1d5904a999c6b94a48d17f2732
Stijn Masschelein <stijnmasschelein@gmail.com>
2018-12-12 15:45:50 +0800

    fix simulation test

--- tests/testthat/test-samples.R
+++ tests/testthat/test-samples.R
@@ -1,21 +1,8 @@
-#
-# context("simulated sample")
-#
-# test_that("No nans in dataset, all variables included", {
-#   practices = 2
-#   exo_factors = 3
-#   observations = 100
-#   sample = create_sample(observations = observations, practices = practices,
-#                        exo_factors = exo_factors)
-#   expect_true(!anyNA(sample))
-#   expect_equal(ncol(sample), practices + exo_factors + 1)
-# })
-
 context("simulated data")
 
 test_that("No nans in dataset, all variables included", {
   observations = 100
-  sample = simcompl::create_sample(obs = observations)
+  sample = simcompl2::create_sample(obs = observations)
   expect_true(!anyNA(sample))
   expect_equal(ncol(sample), 7)
 })
